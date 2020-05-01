6e023194738fe5fc411d0c40e88a8d0903e49f8a
Gabor Dora <g.dora981225@gmail.com>
2020-05-01 22:10:03 +0200

    BookDao osztály elkészítése

--- src/main/java/book/BookDao.java
+++ src/main/java/book/BookDao.java
@@ -1,4 +1,37 @@
 package book;
 
-public class BookDao {
+import book.model.Book;
+import com.google.inject.persist.Transactional;
+import jpa.GenericJpaDao;
+import user.model.User;
+
+import javax.persistence.NoResultException;
+import java.util.Optional;
+
+public class BookDao extends GenericJpaDao<Book> {
+
+    //Optional<Book> findByIsbn13(String isbn13): visszaadja az adatbázisból az adott ISBN számú könyvet
+    // ábrázoló objektumot, ha van ilyen, egyébként egy üres Optional objektumot ad vissza.
+
+    @Transactional
+    public Optional<Book> findByIsbn13(String isbn13){
+        try {
+            return Optional.of(entityManager.createQuery("SELECT b FROM Book b WHERE b.isbn13 = :isbn13", Book.class)
+                    .setParameter("isbn13", isbn13)
+                    //.getSingleResult());  ez kell?
+                    }
+        catch (NoResultException e) {
+            return Optional.empty();
+        }
+    }
+
+    //@Transactional
+    //public Optional<User> findByUsername(String username) {
+        //try {
+            //return Optional.of(entityManager.createQuery("SELECT u FROM User u WHERE u.username = :username", User.class)
+                    //.setParameter("username", username)
+                    //.getSingleResult());
+        //} catch (NoResultException e) {
+            //return Optional.empty();
+        //}
 }
